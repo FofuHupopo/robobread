8ed89c19a42f29489d28868a3fcbf6b530292cdd
FofuHupopo <fofuhupopo@gmail.com>
2024-05-16 00:36:56 +0500

    RM: rm volumes from docker-compose

--- server/server/urls.py
+++ server/server/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
 
 if hasattr(settings, "DEBUG") and getattr(settings, "DEBUG"):
     from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
