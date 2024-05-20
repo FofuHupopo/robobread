712eb0765167bfbe683412564cbe9dfe901a71fc
FofuHupopo <fofuhupopo@gmail.com>
2024-05-21 00:38:28 +0500

    fix

--- server/server/settings.py
+++ server/server/settings.py
@@ -117,6 +117,13 @@ MEDIA_ROOT = BASE_DIR / "media/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://localhost",
+]
+
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
