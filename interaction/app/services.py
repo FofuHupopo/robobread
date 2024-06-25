08f33d687a3ea9075fcbb780ca047ce2cb97638e
FofuHupopo <fofuhupopo@gmail.com>
2024-06-25 22:49:46 +0500

    add stopbit 1.5

--- interaction/app/services.py
+++ interaction/app/services.py
@@ -1,9 +1,10 @@
 from pymodbus.client.serial import ModbusSerialClient
+import serial
 
 
 class ModbusService:
     def __init__(self, port, address):
-        self.client = ModbusSerialClient(method='rtu', port=port, timeout=1, baudrate=9600)
+        self.client = ModbusSerialClient(method='rtu', port=port, timeout=1, baudrate=9600, stopbits=serial.STOPBITS_ONE_POINT_FIVE)
         self.address = address
 
     def connect(self):
@@ -19,9 +20,3 @@ class ModbusService:
     def sell_item(self, cell_number):
         sell_command = [0x10, 0x10, 0x00, 0x00, 0x01, 0x02, 0x00, cell_number, 0x00, 0x00]
         self.write_registers(16, sell_command)
-
-
-# service = ModbusService('/dev/ttyUSB0', 1)
-# service.connect()
-# service.sell_item(3)
-# service.disconnect()
