2aa97c545b1b485fc114fe3ed616e7deabd14b9a
FofuHupopo <fofuhupopo@gmail.com>
2024-06-26 02:12:52 +0500

    add modbus check script

--- interaction/check_modbus.py
+++ interaction/check_modbus.py
@@ -1,4 +1,5 @@
 import minimalmodbus
+import time
 
 
 instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
@@ -10,3 +11,11 @@ instrument.serial.timeout = 1
 instrument.mode = minimalmodbus.MODE_RTU
 
 instrument.write_register(0x1008, 0x0001, functioncode=6)
+
+time.sleep(10)
+
+instrument.write_registers(0x1000, [0x000a])
+
+time.sleep(1)
+
+instrument.write_register(0x1006, 0x0001, functioncode=6)
