7d2c9859cf92595e079190cd47675200fdcb4047
FofuHupopo <fofuhupopo@gmail.com>
2024-06-26 01:40:17 +0500

    add modbus functional

--- interaction/app/services.py
+++ interaction/app/services.py
@@ -1,5 +1,6 @@
 import minimalmodbus
 
+
 class ModbusService:
     def __init__(self, port, address):
         self.instrument = minimalmodbus.Instrument(port, address)
@@ -20,5 +21,26 @@ class ModbusService:
         self.instrument.write_registers(register, values, functioncode=16)
 
     def sell_item(self, cell_number):
-        sell_command = [0x10, 0x10, 0x00, 0x00, 0x01, 0x02, 0x00, cell_number, 0x00, 0x00]
-        self.write_registers(16, sell_command)
+        initial_command = [0x01, 0x06, 0x10, 0x08, 0x00, 0x01, 0xCD, 0x08]
+        sell_command = [0x01, 0x10, 0x10, 0x00, 0x00, 0x01, 0x02, 0x00, cell_number]
+        sell_command.extend(self.calculate_crc16_modbus(sell_command))
+        dispense_command = [0x01, 0x06, 0x10, 0x06, 0x00, 0x01, 0xAC, 0xCB]
+
+        commands = [initial_command, sell_command, dispense_command]
+
+        for command in commands:
+            self.write_registers(16, command)
+            
+    def calculate_crc16_modbus(self, data):
+        crc = 0xFFFF
+        polynomial = 0xA001
+
+        for byte in data:
+            crc ^= byte
+            for _ in range(8):
+                if crc & 0x0001:
+                    crc = (crc >> 1) ^ polynomial
+                else:
+                    crc >>= 1
+
+        return crc.to_bytes(2, byteorder='little')
